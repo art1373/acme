1a823a49f8f32f008b74d5ffd2c745c09dc798c9
art1373 <alireza.tavakol@hemnet.se>
2024-07-19 21:33:12 +0200

    trigger release

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -3,12 +3,6 @@ import { CalendarIcon } from "@heroicons/react/24/outline";
 import { lusitana } from "@/app/ui/fonts";
 import { Revenue } from "@/app/lib/definitions";
 
-// This component is representational only.
-// For data visualization UI, check out:
-// https://www.tremor.so/
-// https://www.chartjs.org/
-// https://airbnb.io/visx/
-
 export default async function RevenueChart({
   revenue,
 }: {
