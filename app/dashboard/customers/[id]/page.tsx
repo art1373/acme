3bc91ce770af6dc23a31dcc2e174c2febe39fd2c
art1373 <alireza.tavakol@hemnet.se>
2024-08-02 13:33:15 +0200

    bread crumbs

--- app/dashboard/customers/[id]/page.tsx
+++ app/dashboard/customers/[id]/page.tsx
@@ -1,14 +1,28 @@
 import { fetchCustomerById, fetchCustomers } from "@/app/lib/data";
 import { notFound } from "next/navigation";
 import CustomerCard from "@/app/ui/customers/customerCard";
+import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 
 export default async function Page({ params }: { params: { id: string } }) {
-  const d = await fetchCustomerById(params.id);
-  if (!d) {
+  const customer = await fetchCustomerById(params.id);
+  if (!customer) {
     notFound();
   }
 
-  console.log({ d });
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: "Customers", href: "/dashboard/customers" },
+          {
+            label: "Edit Invoice",
+            href: `/dashboard/customers/${params.id}`,
+            active: true,
+          },
+        ]}
+      />
 
-  return <CustomerCard customer={d} />;
+      <CustomerCard customer={customer} />
+    </main>
+  );
 }
